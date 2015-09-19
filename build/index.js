6ef266519216a05fc6110fa23d67f8df749c31e9
Charlie Cheever <ccheever@gmail.com>
2015-09-18 19:56:28 -0700

    Adding functionality for choosing and opening programs

--- build/index.js
+++ build/index.js
@@ -4,6 +4,8 @@ var _asyncToGenerator = require('babel-runtime/helpers/async-to-generator')['def
 
 var _Object$assign = require('babel-runtime/core-js/object/assign')['default'];
 
+var _Promise = require('babel-runtime/core-js/promise')['default'];
+
 var osascriptExecAsync = _asyncToGenerator(function* (script, opts) {
   return yield execAsync('osascript', osascriptArgs(script), _Object$assign({ stdio: 'inherit' }, opts));
 });
@@ -26,7 +28,58 @@ var safeIdOfAppAsync = _asyncToGenerator(function* (appName) {
 });
 
 var openFinderToFolderAsync = _asyncToGenerator(function* (dir) {
-  return yield osascriptSpawnAsync(['tell application "Finder"', 'open POSIX file ' + JSON.stringify(dir), 'end tell']);
+  var activate = arguments.length <= 1 || arguments[1] === undefined ? true : arguments[1];
+
+  yield osascriptSpawnAsync(['tell application "Finder"', 'open POSIX file ' + JSON.stringify(dir), activate && 'activate' || '', 'end tell']);
+});
+
+var activateApp = _asyncToGenerator(function* (appName) {
+  return yield osascriptSpawnAsync('tell app ' + JSON.stringify(appName) + ' to activate');
+});
+
+var openInAtom = _asyncToGenerator(function* (pth) {
+  return yield osascriptSpawn('tell app "Atom" to open ' + JSON.stringify(pth));
+});
+
+var openInSublime = _asyncToGenerator(function* (pth) {
+  return yield osascriptSpawn('tell app "Sublime Text" to open ' + JSON.stringify(pth));
+});
+
+var chooseAppAsync = _asyncToGenerator(function* (listOfAppNames) {
+  var runningAwaitables = [];
+  var appIdAwaitables = [];
+  for (var appName of listOfAppNames) {
+    runningAwaitables.push(isAppRunningAsync(appName));
+    appIdAwaitables.push(safeIdOfAppAsync(appName));
+  }
+  var running = yield _Promise.all(runningAwaitables);
+  var appIds = yield _Promise.all(appIdAwaitables);
+
+  var i = undefined;
+  for (i = 0; i < listOfAppNames.length; i++) {
+    if (running[i]) {
+      return listOfAppNames[i];
+    }
+  }
+
+  for (i = 0; i < listOfAppNames.length; i++) {
+    if (!!appIds[i]) {
+      return listOfAppNames[i];
+    }
+  }
+
+  return null;
+});
+
+var chooseEditorAppAsync = _asyncToGenerator(function* () {
+  return yield chooseAppAsync(['Atom', 'Sublime Text', 'TextMate', 'TextWrangler', 'Brackets', 'SubEthaEdit', 'BBEdit', 'Textastic', 'UltraEdit', 'MacVim', 'CodeRunner 2', 'CodeRunner', 'TextEdit']);
+});
+
+var chooseTerminalAppAsync = _asyncToGenerator(function* () {
+  return yield chooseAppAsync(['iTerm',
+  // 'Cathode',
+  // 'Terminator',
+  'MacTerm', 'Terminal']);
 });
 
 var execAsync = require('exec-async');
@@ -48,10 +101,17 @@ function osascriptArgs(script) {
 }
 
 module.exports = {
+  activateApp: activateApp,
+  chooseAppAsync: chooseAppAsync,
+  chooseEditorAppAsync: chooseEditorAppAsync,
+  chooseTerminalAppAsync: chooseTerminalAppAsync,
   execAsync: osascriptExecAsync,
   isAppRunningAsync: isAppRunningAsync,
   openFinderToFolderAsync: openFinderToFolderAsync,
+  openInAtom: openInAtom,
+  openInSublime: openInSublime,
   safeIdOfAppAsync: safeIdOfAppAsync,
   spawnAsync: osascriptSpawnAsync
+
 };
 //# sourceMappingURL=sourcemaps/index.js.map
--- src/index.js
+++ src/index.js
@@ -38,18 +38,93 @@ async function safeIdOfAppAsync(appName) {
   }
 }
 
-async function openFinderToFolderAsync(dir) {
-  return await osascriptSpawnAsync([
+async function openFinderToFolderAsync(dir, activate=true) {
+  await osascriptSpawnAsync([
       'tell application "Finder"',
       'open POSIX file ' + JSON.stringify(dir),
+      (activate && 'activate' || ''),
       'end tell',
   ]);
 }
 
+async function activateApp(appName) {
+  return await osascriptSpawnAsync('tell app ' + JSON.stringify(appName) + ' to activate');
+}
+
+async function openInAtom(pth) {
+  return await osascriptSpawn('tell app "Atom" to open ' + JSON.stringify(pth));
+}
+
+async function openInSublime(pth) {
+  return await osascriptSpawn('tell app "Sublime Text" to open ' + JSON.stringify(pth));
+}
+
+async function chooseAppAsync(listOfAppNames) {
+  let runningAwaitables = [];
+  let appIdAwaitables = [];
+  for (let appName of listOfAppNames) {
+    runningAwaitables.push(isAppRunningAsync(appName));
+    appIdAwaitables.push(safeIdOfAppAsync(appName));
+  }
+  let running = await Promise.all(runningAwaitables);
+  let appIds = await Promise.all(appIdAwaitables);
+
+  let i;
+  for (i = 0; i < listOfAppNames.length; i++) {
+    if (running[i]) {
+      return listOfAppNames[i];
+    }
+  }
+
+  for (i = 0; i < listOfAppNames.length; i++) {
+    if (!!appIds[i]) {
+      return listOfAppNames[i];
+    }
+  }
+
+  return null;
+
+}
+
+async function chooseEditorAppAsync() {
+  return await chooseAppAsync([
+    'Atom',
+    'Sublime Text',
+    'TextMate',
+    'TextWrangler',
+    'Brackets',
+    'SubEthaEdit',
+    'BBEdit',
+    'Textastic',
+    'UltraEdit',
+    'MacVim',
+    'CodeRunner 2',
+    'CodeRunner',
+    'TextEdit',
+  ]);
+}
+
+async function chooseTerminalAppAsync() {
+  return await chooseAppAsync([
+    'iTerm',
+    // 'Cathode',
+    // 'Terminator',
+    // 'MacTerm',
+    'Terminal',
+  ]);
+}
+
 module.exports = {
+  activateApp,
+  chooseAppAsync,
+  chooseEditorAppAsync,
+  chooseTerminalAppAsync,
   execAsync: osascriptExecAsync,
   isAppRunningAsync,
   openFinderToFolderAsync,
+  openInAtom,
+  openInSublime,
   safeIdOfAppAsync,
   spawnAsync: osascriptSpawnAsync,
+
 }
